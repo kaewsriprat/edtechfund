a30add1429bccd881fd2614be95cdf62aeaa9a50
Tong <apichartk@sueksa.go.th>
2023-09-14 11:15:27 +0700

    display error -1: App.php

--- core/App.php
+++ core/App.php
@@ -8,7 +8,7 @@ class App
 
     function __construct()
     {
-        ini_set('display_errors', 0);
+        ini_set('display_errors', -1);
         error_reporting(E_ERROR | E_WARNING | E_PARSE);
 
         define('URI', $_SERVER['REQUEST_URI']);
