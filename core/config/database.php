f69f8c8b129131304b6bec3fa00b436e5fd148df
Tong <apichartk@sueksa.go.th>
2023-09-14 10:52:51 +0700

    databse.php change host

--- core/config/database.php
+++ core/config/database.php
@@ -1,35 +1,37 @@
 <?php
 
+
+$this->config['Wdb'] = array(
+    'driver'   => 'mysql',
+    'host'     => '192.168.20.20',
+    'username' => 'root',
+    'password' => '123qweASD',
+    'name'     => 'edtechfund'
+);
+
+$this->config['Rdb'] = array(
+    'driver'   => 'mysql',
+    'host'     => '192.168.20.20',
+    'username' => 'root',
+    'password' => '123qweASD',
+    'name'     => 'edtechfund'
+);
+
+
 /*
 $this->config['Wdb'] = array(
     'driver'   => 'mysql',
-    'host'     => '192.168.20.20',
+    'host'     => 'localhost',
     'username' => 'root',
-    'password' => '123qweASD',
+    'password' => '123456',
     'name'     => 'edtechfund'
 );
 
 $this->config['Rdb'] = array(
     'driver'   => 'mysql',
-    'host'     => '192.168.20.20',
+    'host'     => 'localhost',
     'username' => 'root',
-    'password' => '123qweASD',
+    'password' => '123456',
     'name'     => 'edtechfund'
 );
 */
-
-$this->config['Wdb'] = array(
-    'driver'   => 'mysql',
-    'host'     => 'localhost',
-    'username' => 'root',
-    'password' => '123456',
-    'name'     => 'edtechfund'
-);
-
-$this->config['Rdb'] = array(
-    'driver'   => 'mysql',
-    'host'     => 'localhost',
-    'username' => 'root',
-    'password' => '123456',
-    'name'     => 'edtechfund'
-);
