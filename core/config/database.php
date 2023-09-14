fe73e63702d0c3c32beb3a11aa92d1ce7b32c3e5
Tong <apichartk@sueksa.go.th>
2023-09-14 11:19:29 +0700

    commit all

--- core/config/database.php
+++ core/config/database.php
@@ -1,26 +1,26 @@
 <?php
 
-
-$this->config['Wdb'] = array(
-    'driver'   => 'mysql',
-    'host'     => '192.168.20.20',
-    'username' => 'root',
-    'password' => '123qweASD',
-    'name'     => 'edtechfund'
-);
-
-$this->config['Rdb'] = array(
-    'driver'   => 'mysql',
-    'host'     => '192.168.20.20',
-    'username' => 'root',
-    'password' => '123qweASD',
-    'name'     => 'edtechfund'
-);
-
-
 /*
 $this->config['Wdb'] = array(
     'driver'   => 'mysql',
+    'host'     => '192.168.20.20',
+    'username' => 'root',
+    'password' => '123qweASD',
+    'name'     => 'edtechfund'
+);
+
+$this->config['Rdb'] = array(
+    'driver'   => 'mysql',
+    'host'     => '192.168.20.20',
+    'username' => 'root',
+    'password' => '123qweASD',
+    'name'     => 'edtechfund'
+);
+*/
+
+
+$this->config['Wdb'] = array(
+    'driver'   => 'mysql',
     'host'     => 'localhost',
     'username' => 'root',
     'password' => '123456',
@@ -34,4 +34,3 @@ $this->config['Rdb'] = array(
     'password' => '123456',
     'name'     => 'edtechfund'
 );
-*/
