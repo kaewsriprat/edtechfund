fabbc30596b128de586cf0709b4f7b02f3c39eb7
Tong <apichartk@sueksa.go.th>
2023-09-14 10:32:34 +0700

    home.php period-4 value changed

--- core/config/database.php
+++ core/config/database.php
@@ -1,24 +1,24 @@
 <?php
+
+$this->config['Wdb'] = array(
+    'driver'   => 'mysql',
+    'host'     => '192.168.20.20',
+    'username' => 'root',
+    'password' => '123qweASd',
+    'name'     => 'edtechfund'
+);
+
+$this->config['Rdb'] = array(
+    'driver'   => 'mysql',
+    'host'     => '192.168.20.20',
+    'username' => 'root',
+    'password' => '123qweASd',
+    'name'     => 'edtechfund'
+);
+
 /*
 $this->config['Wdb'] = array(
     'driver'   => 'mysql',
-    'host'     => '192.168.20.20',
-    'username' => 'root',
-    'password' => '123qweASd',
-    'name'     => 'edtechfund'
-);
-
-$this->config['Rdb'] = array(
-    'driver'   => 'mysql',
-    'host'     => '192.168.20.20',
-    'username' => 'root',
-    'password' => '123qweASd',
-    'name'     => 'edtechfund'
-);*/
-
-
-$this->config['Wdb'] = array(
-    'driver'   => 'mysql',
     'host'     => 'localhost',
     'username' => 'root',
     'password' => '123456',
@@ -32,3 +32,4 @@ $this->config['Rdb'] = array(
     'password' => '123456',
     'name'     => 'edtechfund'
 );
+*/
